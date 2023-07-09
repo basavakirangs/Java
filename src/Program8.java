20441093196d3462c402b77a1ddbacb46c3c64fe
basavakiran-gs <gsbasavakiran@gmail.com>
2023-07-09 17:55:08 +0530

    Adding few more programs

---
+++ src/Program8.java
@@ -0,0 +1,27 @@
+//write a java program to Find the sum of the all odd index elements in an Array
+import java.util.Scanner;
+public class Program8 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the size of the Array");
+		int size = scan.nextInt();
+		int arr[] = new int[size];
+		int sum =0;
+		
+		System.out.println("Enter the elements of the Array");
+		for (int i=0;i<arr.length;i++){
+			arr[i] = scan.nextInt();
+		}
+		System.out.println("Printing the sum of the all odd index elements in an Array");
+		for (int i=0;i<arr.length;i++) {
+			if(i%2 != 0) {
+			sum += arr[i];
+			}
+		}
+		System.out.println(sum);
+		
+
+	}
+
+}
